66d20ac27f7ec84de952f548e17123cc8406a142
Willistic <jlubbick@aol.com>
2022-12-05 18:34:35 +0100

    search

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,7 +6,7 @@ function Card({ hotels }) {
     <div className='grid grid-cols-1 gap-5 p-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
         {hotels.map((data) => (
             <article className='bg-white shadow p-5' key={data.listing_id}>
-                <button style={{backgroundColor: "#efc713", Color: "white", textAlign: "right"}}>On Map</button>
+                <button style={{backgroundColor: "#efc713", float: "right"}}>On Map</button>
                 <h3 className='text-2xl mb-3'>{data.company}</h3>
                 <p>{data.city}</p>
                 <p>{data.phone}</p>
